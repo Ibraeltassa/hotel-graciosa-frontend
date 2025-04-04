23614f3d36b73316c71a25f02814f945b8269df3
Ibrahim <ibrahimeltassa@gmail.com>
2025-04-04 16:36:16 -0300

    feat: estrutura inicial com componentes Header e Home

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <header style={{ background: '#222', padding: '10px', color: '#fff' }}>
+            <h1>Hotel Graciosa</h1>
+        </header>
+
+    )
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <main> style={{ padding: '20px' }}
+            <h2>Bem-vindo ao sistema do Hotel Graciosa</h2>
+            <p>Essa será a página inicial do sistema</p>
+        </main>
+    );
+}
+
+export default Home;
